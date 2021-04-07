a9a118ad5a86d8d6c78e7bbaf9f62de2ab920701
layanabushaweesh <layanabushaweesh@gmail.com>
2021-04-07 15:23:22 +0300

    update

---
+++ data/seeds.sql
@@ -0,0 +1,47 @@
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'Dune',
+  'Frank Herbert',
+  'ISBN_13 9780441013593',
+  'http://books.google.com/books/content?id=B1hSG45JCX4C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Follows the adventures of Paul Atreides, the son of a betrayed duke given up for dead on a treacherous desert planet and adopted by its fierce, nomadic people, who help him unravel his most unexpected destiny.'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'What Alice Forgot',
+  'Liane Moriarty',
+  'ISBN_13 1101515376',
+  'http://books.google.com/books/content?id=8iBGzeqj45YC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Alice Love is twenty-nine, crazy about her husband, and pregnant with her first child. So imagine Alice''s surprise when she comes to on the floor of a gym (a gym! She HATES the gym) and is whisked off to the hospital where she discovers the honeymoon is truly over—she''s getting divorced, she has three kids, and she''s actually 39 years old. Alice must reconstruct the events of a lost decade, and find out whether it''s possible to reconstruct her life at the same time. She has to figure out why her sister hardly talks to her, and how is it that she''s become one of those super skinny moms with really expensive clothes. Ultimately, Alice must discover whether forgetting is a blessing or a curse, and whether it''s possible to start over...'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'Unsouled',
+  'Will Wight',
+  'ISBN_13 9780989671767',
+  'http://books.google.com/books/content?id=OjYJtAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+  'Sacred artists follow a thousand Paths to power, using their souls to control the forces of the natural world. Lindon is Unsouled, forbidden to learn the sacred arts of his clan. When faced with a looming fate he cannot ignore, he must rise beyond anything he''s ever known...and forge his own Path.'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'The Stand',
+  'Stephen King',
+  'ISBN_13 9780385528856',
+  'http://books.google.com/books/content?id=UbfnTcmkaKkC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Stephen King''s apocalyptic vision of a world blasted by plague and tangled in an elemental struggle between good and evil remains as riveting and eerily plausible as when it was first published. A patient escapes from a biological testing facility, unknowingly carrying a deadly weapon: a mutated strain of super-flu that will wipe out 99 percent of the world''s population within a few weeks. Those who remain are scared, bewildered, and in need of a leader. Two emerge—Mother Abagail, the benevolent 108-year-old woman who urges them to build a peaceful community in Boulder, Colorado; and Randall Flagg, the nefarious "Dark Man," who delights in chaos and violence. As the dark man and the peaceful woman gather power, the survivors will have to choose between them—and ultimately decide the fate of all humanity.'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'Sweet Thursday',
+  'John Steinbeck',
+  'ISBN_13 978144063549',
+  'http://books.google.com/books/content?id=zer-bEoDL-EC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'In Monterey, on the California coast, Sweet Thursday is what they call the day after Lousy Wednesday, which is one of those days that are just naturally bad. Returning to the scene of Cannery Row—the weedy lots and junk heaps and flophouses of Monterey, John Steinbeck once more brings to life the denizens of a netherworld of laughter and tears—from Doc, based on Steinbeck''s lifelong friend Ed Ricketts, to Fauna, new headmistress of the local brothel, to Hazel, a bum whose mother must have wanted a daughter. This Penguin Classics edition features an introduction and notes by Robert DeMott. For more than seventy years, Penguin has been the leading publisher of classic literature in the English-speaking world. With more than 1,700 titles, Penguin Classics represents a global bookbookshelf of the best works throughout history and across genres and disciplines. Readers trust the series to provide authoritative texts enhanced by introductions and notes by distinguished scholars and contemporary authors, as well as up-to-date translations by award-winning translators. From the Trade Paperback edition.'
+);
+
+INSERT INTO books (title, author, isbn, image_url, description) VALUES (
+  'The New Jim Crow',
+  'Michelle Alexander',
+  'ISBN_13 9781595588197',
+  'http://books.google.com/books/content?id=reDzBZ3pXqsC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  'Once in a great while a book comes along that changes the way we see the world and helps to fuel a nationwide social movement. The New Jim Crow is such a book. Praised by Harvard Law professor Lani Guinier as "brave and bold,"" this book directly challenges the notion that the election of Barack Obama signals a new era of colorblindness. With dazzling candor, legal scholar Michelle Alexander argues that "we have not ended racial caste in America; we have merely redesigned it." By targeting black men through the War on Drugs and decimating communities of color, the U.S. criminal justice system functions as a contemporary system of racial control—relegating millions to a permanent second-class status—even as it formally adheres to the principle of colorblindness. In the words of Benjamin Todd Jealous, president and CEO of the NAACP, this book is a "call to action." Called "stunning" by Pulitzer Prize–winning historian David Levering Lewis, "invaluable" by the Daily Kos, "explosive" by Kirkus, and "profoundly necessary" by the Miami Herald, this updated and revised paperback edition of The New Jim Crow, now with a foreword by Cornel West, is a must-read for all people of conscience.'
+);
